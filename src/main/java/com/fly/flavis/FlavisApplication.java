1a9115e7ff3b645b69cbd0437b5799f5e4586b38
otaku904 <domu904@gmail.com>
2020-07-11 20:35:54 -0500

    init

---
+++ src/main/java/com/fly/flavis/FlavisApplication.java
@@ -0,0 +1,13 @@
+package com.fly.flavis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlavisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlavisApplication.class, args);
+	}
+
+}
