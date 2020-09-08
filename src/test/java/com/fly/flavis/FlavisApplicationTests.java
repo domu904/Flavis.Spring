fed84c5257b2a60036dadc61c0fdce12448f62ed
otaku904 <domu904@gmail.com>
2020-09-07 20:19:51 -0500

    Updated Test Cases

--- src/test/java/com/fly/flavis/FlavisApplicationTests.java
+++ src/test/java/com/fly/flavis/FlavisApplicationTests.java
@@ -3,6 +3,7 @@ package com.fly.flavis;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class FlavisApplicationTests {
