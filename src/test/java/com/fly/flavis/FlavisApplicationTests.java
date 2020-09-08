8d475657c2a4061a3eed73852225131fd85d5c47
otaku904 <domu904@gmail.com>
2020-09-07 20:16:02 -0500

    Added more test cases

--- src/test/java/com/fly/flavis/FlavisApplicationTests.java
+++ src/test/java/com/fly/flavis/FlavisApplicationTests.java
@@ -1,13 +1,18 @@
 package com.fly.flavis;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FlavisApplicationTests {
 
+	@Autowired
+	private FlavisApplication controller;
+
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
