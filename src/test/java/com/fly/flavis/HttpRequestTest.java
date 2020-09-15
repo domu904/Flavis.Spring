4d91f9ef0257626745ee7cfbabb78bd12e7ec7f1
otaku904 <domu904@gmail.com>
2020-09-14 22:16:19 -0500

    Update HttpRequestTest.java

--- src/test/java/com/fly/flavis/HttpRequestTest.java
+++ src/test/java/com/fly/flavis/HttpRequestTest.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+import java.io.IOException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -19,5 +23,20 @@ public class HttpRequestTest {
     public void greetingShouldReturnDefaultMessage() throws Exception {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
                 String.class)).contains("Hello, World");
+    }
+
+    @Test
+    public void givenUserDoesNotExists_whenUserInfoIsRetrieved_then404IsReceived()
+            throws ClientProtocolException, IOException {
+
+        // Given
+        String name = RandomStringUtils.randomAlphabetic( 8 );
+        HttpUriRequest request = new HttpGet( "https://api.github.com/users/" + name );
+
+        // When
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute( request );
+
+        // Then
+        assertThat(httpResponse.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_NOT_FOUND));
     }
 }
