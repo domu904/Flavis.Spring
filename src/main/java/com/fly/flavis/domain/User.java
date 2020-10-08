3e4ce7df2052cf0e7f3bd49018952be0cbd0fb70
otaku904 <domu904@gmail.com>
2020-10-07 21:44:35 -0500

    Create User.java

---
+++ src/main/java/com/fly/flavis/domain/User.java
@@ -0,0 +1,11 @@
+package com.fly.flavis.domain;
+
+public class User {
+
+    private long id;
+    private String email;
+    private String fName;
+    private String lName;
+    private String isActive;
+
+}
