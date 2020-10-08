521a47dbde5fc3896b395b85439c731b0cac7523
otaku904 <domu904@gmail.com>
2020-10-07 21:39:34 -0500

    Create FlavisControllerTest.java

---
+++ src/test/java/com/fly/flavis/controller/FlavisControllerTest.java
@@ -0,0 +1,13 @@
+package com.fly.flavis.controller;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FlavisControllerTest {
+
+    @Test
+    public void Test_User(){
+        assertTrue(true);
+    }
+
+}
