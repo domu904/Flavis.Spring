e899f99628a66d5acf4d969013ac9895f2e3f703
otaku904 <domu904@gmail.com>
2020-10-08 22:39:29 -0500

    Create UserTest.java

---
+++ src/test/java/com/fly/flavis/domain/UserTest.java
@@ -0,0 +1,16 @@
+package com.fly.flavis.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UserTest {
+
+    private User user = new User();
+
+    @Test
+    public void test_fName(){
+        assertTrue(false);
+    }
+
+}
