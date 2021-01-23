6402b14fd581fa91a2196f6bc478741bfa6c2da9
otaku904 <domu904@gmail.com>
2021-01-22 22:19:56 -0600

    Update FlavisController.java

--- src/main/java/com/fly/flavis/controller/FlavisController.java
+++ src/main/java/com/fly/flavis/controller/FlavisController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FlavisController {
 
-    @GetMapping("user")
+    @GetMapping("/user")
     public String getUser(long id) {
         return "user1";
     }
