04a60ab12520233c9022362657b9a49a60952273
otaku904 <domu904@gmail.com>
2021-01-22 23:25:36 -0600

    Update FlavisControllerTest.java

--- src/test/java/com/fly/flavis/controller/FlavisControllerTest.java
+++ src/test/java/com/fly/flavis/controller/FlavisControllerTest.java
@@ -1,12 +1,17 @@
 package com.fly.flavis.controller;
 
+import com.fly.flavis.domain.User;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FlavisControllerTest {
 
+    @autowired
+    User user;
+
     @Test
     public void test_getUser(){
+
         assertTrue(false);
     }
 
